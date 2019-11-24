9564ec2ed554e0b4b473eb53699f9ed6bb5dccca
czlinlin <linlin198703@163.com>
2019-11-24 22:32:08 +0800

    初始导入

---
+++ 作品/files/预约成功_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
+var b="url",c="预约成功_1.html",d="generationDate",e=new Date(1574594302435.71),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="Kilometres",k="arrivalTime",l="waitting_at_store",m="car_Wash",n="haveMeals",o="backupText",p="page",q="packageId",r="4d28da4104e4492dad1de7aed1db0b4c",s="type",t="Axure:Page",u="name",v="预约成功",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
+return _creator();
+})());
